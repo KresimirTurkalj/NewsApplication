0f49e182ea22ff049d750466eccfd64a8fb1c85d
Krešimir Turkalj <kreso005@gmail.com>
2020-03-31 01:35:45 +0200

    Revert "Revert "Revert "Remade Splash screen"""
    This reverts commit 1734ace0d9159c3b8f019382b47521a327e71342.

--- app/src/main/java/com/example/newsapplication/recyclerview/NewsRecyclerViewHolder.kt
+++ app/src/main/java/com/example/newsapplication/recyclerview/NewsRecyclerViewHolder.kt
@@ -1,4 +1,4 @@
-package com.example.newsapplication.newsdisplay.recyclerview
+package com.example.newsapplication.recyclerview
 
 import android.view.View
 import android.widget.ImageView
@@ -6,7 +6,7 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.newsapplication.R
 
-class NewsRecyclerViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+class NewsRecyclerViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
     val articleImageView = view.findViewById(R.id.image_view_article) as ImageView
     val articleTextView = view.findViewById(R.id.text_view_article) as TextView
 }
